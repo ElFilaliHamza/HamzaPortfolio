70383f29e4c3a45d35c23fa4928c2d13cb6234ef
HamzaFun <79369358+HamzaFun@users.noreply.github.com>
2022-02-12 22:09:17 +0100

    porfolio project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>My Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
